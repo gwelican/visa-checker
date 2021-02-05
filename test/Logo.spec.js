161a5c57697855405d459ddb4256924a55e7fdcf
Peter Varsanyi <superfly@gwelican.eu>
2021-02-05 00:36:03 -0800

    add test for l1visa

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
-// import { mount } from '@vue/test-utils'
-// import Logo from '@/components/Logo.vue'
+import { mount } from '@vue/test-utils'
+import L1Visa from '@/components/L1.vue'
 
-// describe('Logo', () => {
-//   test('is a Vue instance', () => {
-//     const wrapper = mount(Logo)
-//     expect(wrapper.vm).toBeTruthy()
-//   })
-// })
+describe('L1Visa', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(L1Visa)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
