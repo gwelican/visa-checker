10ceaba0fb9ab6f3269ce7d73ef45b8859b876db
zsolt.domozi <>
2021-02-05 01:50:34 +0100

    Basic data upload - EB2NIW category

--- components/Eb2Niw.vue
+++ components/Eb2Niw.vue
@@ -7,14 +7,33 @@
   </v-card>
 </template>
 <script lang="ts">
-import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import { UserDetails } from '~/pages/index.vue'
+  import { Component, Prop, Vue } from 'nuxt-property-decorator'
+  import { UserDetails } from '~/pages/index.vue'
 
-@Component
-export default class EB2Niw extends Vue {
+  @Component
+  export default class EB2Niw extends Vue {
   @Prop()
   userDetails!: UserDetails
 
-  private requirements = []
-}
+  private requirements = [
+  'Minimum MSc VAGY PhD végzettség',
+  '5 év munkatapasztalat végzettségnek megfelelő területen',
+  '',
+  'VAGY',
+  '',
+  'Kivételes képesség:',
+  '- tudományokban',
+  '- mávészetekben',
+  '- üzleti életben',
+  'Kivételes képesség: "jelentősen meghaladja az adott területen tapasztalható szintet" ',
+  'Munkaajánlat szükséges USA cégtől',
+  'Minimum 3 kritérium teljesítése a felsorolt 7-ből "*Criteria" fejezetben:',
+  '',
+  'PLUSZ',
+  'Bizonyítékok arra vonatkozólag, hogy állami érdek a jelölt beengedése még munkaajánlat nélkül is',
+  '',
+  '',
+
+  ]
+  }
 </script>
